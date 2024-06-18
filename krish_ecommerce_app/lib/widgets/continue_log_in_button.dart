891743501eb828923d4127b3337b21c2a07fe922
Israel Breta <i_israel.breta@stratpoint.com>
2024-06-19 06:12:20 +0800

    Continue log in button

---
+++ krish_ecommerce_app/lib/widgets/continue_log_in_button.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:sign_in_button/sign_in_button.dart';
+
+class ContinueLogInButton extends StatelessWidget {
+  const ContinueLogInButton({super.key, required this.icon});
+  final Buttons icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: SignInButton(
+        icon,
+        onPressed: () {},
+      ),
+    );
+  }
+}
