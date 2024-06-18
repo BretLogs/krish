197ec718b1296a3846ae0d0be1e7f692ff4086f7
Israel Breta <i_israel.breta@stratpoint.com>
2024-06-19 06:14:28 +0800

    Bone Design for Log in page

--- krish_ecommerce_app/lib/pages/login/login_page.dart
+++ krish_ecommerce_app/lib/pages/login/login_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:krish_ecommerce_app/consts.dart';
+import 'package:krish_ecommerce_app/widgets/continue_log_in_button.dart';
 import 'package:krish_ecommerce_app/widgets/primary_button.dart';
 import 'package:krish_ecommerce_app/widgets/log_in_form_field.dart';
+import 'package:sign_in_button/sign_in_button.dart';
 
 class LogInPage extends StatelessWidget {
   const LogInPage({super.key});
@@ -10,25 +12,31 @@ class LogInPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Form(
-              child: Column(
-        children: <Widget>[
-          const Text('Sign In'),
-          LogInFormField(
-            hintText: 'Email Address',
-            height: MediaQuery.sizeOf(context).height * .1,
-            validationRegExp: EMAIL_VALIDATION_REGEX,
-            onSaved: (String? value) {},
-          ),
-          const PrimaryButton(),
-          const Row(
-            children: <Widget>[
-              Text("Don't have an Account? "),
-              Text('Create One'),
-            ],
-          ),
-        ],
-      ))),
+          child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Form(
+            child: Column(
+          children: <Widget>[
+            const Text('Sign In'),
+            LogInFormField(
+              hintText: 'Email Address',
+              height: MediaQuery.sizeOf(context).height * .1,
+              validationRegExp: EMAIL_VALIDATION_REGEX,
+              onSaved: (String? value) {},
+            ),
+            const PrimaryButton(),
+            const Row(
+              children: <Widget>[
+                Text("Don't have an Account? "),
+                Text('Create One'),
+              ],
+            ),
+            const ContinueLogInButton(icon: Buttons.apple),
+            const ContinueLogInButton(icon: Buttons.email),
+            const ContinueLogInButton(icon: Buttons.facebook),
+          ],
+        )),
+      )),
     );
   }
 }
