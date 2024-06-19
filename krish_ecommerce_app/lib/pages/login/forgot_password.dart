5ad84db13d78df17c48796881cf080fdccc84c3e
Israel Breta <i_israel.breta@stratpoint.com>
2024-06-19 09:46:00 +0800

    Navigation fixed forgot password, log in passwod, and password reset

--- krish_ecommerce_app/lib/pages/login/forgot_password.dart
+++ krish_ecommerce_app/lib/pages/login/forgot_password.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:krish_ecommerce_app/consts.dart';
-import 'package:krish_ecommerce_app/pages/login/forgot_password.dart';
+import 'package:krish_ecommerce_app/pages/login/log_in_password.dart';
 import 'package:krish_ecommerce_app/widgets/log_in_form_field.dart';
 import 'package:krish_ecommerce_app/widgets/primary_button.dart';
 
-class LogInPassword extends StatelessWidget {
-  const LogInPassword({super.key});
+class ForgotPassword extends StatelessWidget {
+  const ForgotPassword({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,37 +21,26 @@ class LogInPassword extends StatelessWidget {
             children: <Widget>[
               const SizedBox(height: 80),
               Text(
-                'Sign in',
+                'Forgot Password',
                 style: Theme.of(context).textTheme.headlineMedium!.copyWith(fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 20),
               LogInFormField(
-                hintText: 'Password',
+                hintText: 'Enter Email Address',
                 validationRegExp: PASSWORD_VALIDATION_REGEX,
                 onSaved: (String? value) {},
-                obscureText: true,
               ),
               const SizedBox(height: 8),
               PrimaryButton(
                 text: 'Continue',
-                onTap: () {},
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (BuildContext context) => const LogInPassword()),
+                  );
+                },
               ),
               const SizedBox(height: 12),
-              Row(
-                children: <Widget>[
-                  Text('Forgot Password? ', style: Theme.of(context).textTheme.bodySmall),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (BuildContext context) => const ForgotPassword()),
-                      );
-                    },
-                    child: Text('Reset', style: Theme.of(context).textTheme.bodySmall!.copyWith(fontWeight: FontWeight.w600)),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 40),
             ],
           )),
         ),
